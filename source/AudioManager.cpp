087eba09ceb4c3a71a38554507c84faa7f846bf3
29888146 <29888146@sonoranschools.org>
2019-11-01 13:48:01 -0400

    update AudioManager

--- source/AudioManager.cpp
+++ source/AudioManager.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "AudioManager.h"
+
+  AudioManager(int frequency = 44100, int channels = 2, int chunk_size = 2048)
+  {
+    Mix_OpenAudio(frequency,MIX_DEFAULT_FORMAT,channels,chunk_size);
+  }
+  void AudioManager::LoadMusicFromFile(std::string name, std::string path)
+  {
+    Mix_Music* tempMusic = Mix_LoadMUS(path);
+    this->music_list.insert(new std::pair<std:string,Mix_Music*>(name,tempMusic));
+    Mix_FreeMusic(tempMusic);
+  }
+  void AudioManager::LoadSFXFromFile(std::string name, std::string path)
+  {
+    Mix_Chunk* tempSFX = Mix_LoadWAV(path);
+    this->sfx_list.insert(new std::pair<std::string,Mix_Chunk*>(name,tempSFX));
+    Mix_FreeChunk(tempSFX);
+  }
+  void AudioManager::PlaySFX(std::string name, int channel_number)
+  {
+    Mix_PlayChannel(-1,this->music_list.find(name)->first,channel_number);
+  }
+  void AudioManager::PlayMusic(std::string name, int channel_number)
+  {
+    
+  }
+
+void AudioManager::PauseMusic(std::string name);
+{
+
+}
+  void AudioManager::StopMusic(std::string name)
+{
+    
+}
--- source/AudioManager.h
+++ source/AudioManager.h
@@ -11,9 +11,14 @@ public:
   AudioManager();
   void AudioManager::LoadMusicFromFile(std::string name, std::string path);
   void AudioManager::LoadSFXFromFile(std::string name, std::string path);
+  void AudioManager::PlaySFX(std::string name, int channel_number);
+  void AudioManager::PlayMusic(std::string name, int channel_number);
+  void AudioManager::PauseMusic(std::string name);
+  void AudioManager::StopMusic(std::string name);
 private:
   std::map<std::string,Mix_Music*> music_list;
   std::map<std::string,Mix_Chunk> sfx_list;
+  
 };
 
 
